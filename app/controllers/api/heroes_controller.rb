3144cc5be72a35718f84c1235e20968cecff902b
Elton Santos <eltonaxl@hotmail.com>
2021-12-24 05:10:40 -0300

    Removendo bug

--- app/controllers/api/heroes_controller.rb
+++ app/controllers/api/heroes_controller.rb
@@ -49,7 +49,7 @@ class Api::HeroesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_hero
-      @hero = Hero.by_token(token).find(params[:id])
+      @hero = Hero.by_token(@token).find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2021_09_11_142734) do
 
   create_table "heroes", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "token", null: false
